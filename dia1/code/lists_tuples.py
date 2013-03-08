65201ee6d8ee5ff4383781a7133e97943f0c5977
Maximiliano Guerra de Medeiros <maxciencomp@gmail.com>
2013-03-08 18:58:48 -0300

    Melhorando exemplos das listas

--- dia1/code/lists_tuples.py
+++ dia1/code/lists_tuples.py
@@ -1,15 +1,15 @@
-l = ['laranja', 'pera', 'banana']
+l = ['laranja', 'pera', 5]
 # Adiciona o elemento 'uva' a lista
 l.append('uva')
 # Remove o ultimo elemento e o retorna
-l.pop()
+x = l.pop()
 l[0]   # Retorna o primeiro elemento
-l[-1]  # Retorna o último elemento
+l[-1]  # Retorna o ultimo elemento
 l[1:3] # Retorna uma fatia da lista
 # Do elemento de indice 1
 # ate o elemento com indice anterior a 3
 
-# A tupla é como uma lista somente de leitura
+# A tupla eh como uma lista somente de leitura
 t = (1, 2, 3)
 t[2]
 t[0:2]
